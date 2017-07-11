14bc073c5d12c45a5728167b84bfb0ed3a242917
keith <keith@keithdeMacBook-Pro.local>
2017-07-11 14:57:10 +0800

    kotlin study

---
+++ src/app.kt
@@ -0,0 +1,48 @@
+import java.time.Instant
+
+/**
+ * Created by keith on 2017/7/11.
+ */
+
+fun main(args: Array<String>) {
+
+//    println("Hello Kotlin!")
+//
+//    for (i in 1..10)
+//        println(i)
+//
+//    println("\n----------------------------")
+//
+//    for (i in 1..10 step 2)
+//        println(i)
+//
+//    println("\n----------------------------")
+//
+//    for (i in 10 downTo 1 step 3)
+//        print(i)
+//
+//    println("\n----------------------------")
+//
+//    for (i in 1 until 10)
+//        print(i)
+//
+//    println("\n----------------------------")
+//
+//
+//    var a : Int = 10000
+//    println( a === a )
+//
+//    var boxedA : Int? = a
+//    var boxedB : Int? = a
+//
+//    println(boxedA == boxedB)
+//
+//    println(boxedA === boxedB)
+
+    val arr = arrayOf(1,2,3,4)
+
+    val b = Array(4, { i -> (i * 2) })
+
+    println(arr[0])
+    println(b[1])
+}
